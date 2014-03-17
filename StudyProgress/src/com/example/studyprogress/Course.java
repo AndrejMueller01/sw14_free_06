101f09a5203b633fc9827fd39c2a7889e236be95
Dominik Hirner <dominik.hirner@student.tugraz.at>
2014-03-17 15:09:14 +0100

    StudentProjectCreated

---
+++ StudyProgress/src/com/example/studyprogress/Course.java
@@ -0,0 +1,15 @@
+package com.example.studyprogress;
+
+public class Course {
+	private String courseName;
+	private int ects;
+	private String courseNumber;
+	public Course(String courseName, int ects, String courseNumber)
+	{
+		this.courseName = courseName;
+		this.ects = ects;
+		this.courseNumber = courseNumber;
+	}
+
+	
+}
---
+++ StudyProgress/src/com/example/studyprogress/CurriculumView.java
@@ -0,0 +1,23 @@
+package com.example.studyprogress;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CurriculumView extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_curriculum_view);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
